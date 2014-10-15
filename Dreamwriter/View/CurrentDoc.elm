a3de401296b56b68fe22dc858fc55bc1018622f5
Richard Feldman <richard.t.feldman@gmail.com>
2014-10-15 09:43:24 -0700

    Extract logic specific to displaying current doc in left sidebar into View.CurrentDoc

---
+++ Dreamwriter/View/CurrentDoc.elm
@@ -0,0 +1,48 @@
+module Dreamwriter.View.CurrentDoc (view) where
+
+import Dreamwriter.Doc (..)
+import Dreamwriter.Action (..)
+
+import Html (..)
+import Html.Attributes (..)
+import Html.Events (..)
+import Html.Tags (..)
+
+import Regex (..)
+
+-- TODO remove this once it's in elm-html
+key = attr "key"
+
+-- Replace illegal filename characters with underscores
+illegalFilenameCharMatcher = regex "[/\\<>?|\":*]"
+
+legalizeFilename : String -> String
+legalizeFilename = replace All illegalFilenameCharMatcher (\_ -> "_")
+
+downloadContentType = "text/plain;charset=UTF-8"
+
+-- TODO refactor this to return Html instead of [Html] once vdom bug is fixed...
+view : Doc -> [Html]
+view currentDoc =
+  let downloadOptions = {
+    filename    = (legalizeFilename currentDoc.title) ++ ".html",
+    contentType = downloadContentType
+  }
+  in
+    [
+      div [key "#title", id "title"] [text currentDoc.title],
+      div [key "#file-buttons", id "file-buttons"] [
+        span [key "#download-button", class "file-button",
+          onclick downloadInput.handle (always downloadOptions)] [text "download"],
+        span [key "#stats-button", class "file-button"] [text "stats"]
+      ],
+
+      viewOutline currentDoc.chapters
+    ]
+
+viewOutline : [Chapter] -> Html
+viewOutline chapters =
+  ul [id "outline", key "outline"] <| indexedMap viewChapter chapters
+
+viewChapter : Int -> Chapter -> Html
+viewChapter index chapter = li [key ("chapter" ++ (show index))] [text chapter.heading]
--- Dreamwriter/View/LeftSidebar.elm
+++ Dreamwriter/View/LeftSidebar.elm
@@ -3,10 +3,9 @@ module Dreamwriter.View.LeftSidebar (view) where
 import Dreamwriter.Action (..)
 import Dreamwriter.Model (..)
 import Dreamwriter.Doc (..)
-import Dreamwriter.View.Outline as Outline
+import Dreamwriter.View.CurrentDoc as CurrentDoc
 import Dreamwriter.View.OpenMenu as OpenMenu
 
-import Regex (..)
 import Html (..)
 import Html.Attributes (..)
 import Html.Events (..)
@@ -15,14 +14,6 @@ import Maybe
 
 -- TODO remove this once it's in elm-html
 key = attr "key"
-
--- Replace illegal filename characters with underscores
-illegalFilenameCharMatcher = regex "[/\\<>?|\":*]"
-
-legalizeFilename : String -> String
-legalizeFilename = replace All illegalFilenameCharMatcher (\_ -> "_")
-
-downloadContentType = "text/plain;charset=UTF-8"
 
 view : AppState -> Html
 view state =
@@ -47,27 +38,10 @@ view state =
                 onclick actions.handle (\_ -> ToggleOpenMenu True)] [text "open"]
             ]
 
-          , bodyNodes = viewCurrentDoc currentDoc
+          , bodyNodes = CurrentDoc.view currentDoc
           }
       in
         div [key "#left-sidebar-container", id "left-sidebar-container", class "sidebar"] [
           div [key "#left-sidebar-header", id "left-sidebar-header", class "sidebar-header"] headerNodes,
           div [key "#left-sidebar-body", id "left-sidebar-body"] bodyNodes
         ]
-
-viewCurrentDoc : Doc -> [Html]
-viewCurrentDoc currentDoc =
-  let downloadOptions = { filename    = (legalizeFilename currentDoc.title) ++ ".html"
-                        , contentType = downloadContentType
-                        }
-  in
-    [
-      div [key "#title", id "title"] [text currentDoc.title],
-      div [key "#file-buttons", id "file-buttons"] [
-        span [key "#download-button", class "file-button",
-          onclick downloadInput.handle (always downloadOptions)] [text "download"],
-        span [key "#stats-button", class "file-button"] [text "stats"]
-      ],
-
-      Outline.view currentDoc.chapters
-    ]
