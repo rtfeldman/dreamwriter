bd2beb936381611338c9bf79cdd66a94e65fedd1
Richard Feldman <richard.t.feldman@gmail.com>
2014-10-21 03:04:36 -0700

    Add a basic Notes search bar on the right.

--- Dreamwriter/View/RightSidebar.elm
+++ Dreamwriter/View/RightSidebar.elm
@@ -10,4 +10,12 @@ import Maybe
 
 view : Doc -> AppState -> Html
 view currentDoc state =
-  div [id "right-sidebar-container", class "sidebar"] []
+  div [id "right-sidebar-container", class "sidebar"] [
+    div [id "right-sidebar-header", class "sidebar-header"] [
+      input [id "notes-search-text", placeholder "Search Notes"] [],
+      span [id "notes-search-button", class "sidebar-header-control flaticon-ribbon"] []
+    ],
+    div [id "right-sidebar-body"] [
+      div [id "notes"] []
+    ]
+  ]
