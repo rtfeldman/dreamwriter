e5cc54e69bea7ff0dd7ebe60a4846bb3548e9931
Richard Feldman <richard.t.feldman@gmail.com>
2014-10-21 03:21:06 -0700

    Style notes search bar like the other header elements.

--- Dreamwriter/View/RightSidebar.elm
+++ Dreamwriter/View/RightSidebar.elm
@@ -12,7 +12,7 @@ view : Doc -> AppState -> Html
 view currentDoc state =
   div [id "right-sidebar-container", class "sidebar"] [
     div [id "right-sidebar-header", class "sidebar-header"] [
-      input [id "notes-search-text", placeholder "Search Notes"] [],
+      input [id "notes-search-text", class "sidebar-header-control", placeholder "search notes"] [],
       span [id "notes-search-button", class "sidebar-header-control flaticon-ribbon"] []
     ],
     div [id "right-sidebar-body"] [
