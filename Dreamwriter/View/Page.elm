26af86d66ec6f8987d8e4688d754eeea651d42fe
Richard Feldman <richard.t.feldman@gmail.com>
2014-11-20 19:03:28 -0800

    Remove backdrop.

--- Dreamwriter/View/Page.elm
+++ Dreamwriter/View/Page.elm
@@ -14,14 +14,7 @@ import Html.Tags (..)
 view : AppState -> Html
 view state =
   div [id "page"] <|
-    [
-      div [class "backdrop"] [
-        div [class "backdrop-quadrant backdrop-top backdrop-left"] [],
-        div [class "backdrop-quadrant backdrop-bottom backdrop-left"] [],
-        div [class "backdrop-quadrant backdrop-top backdrop-right"] [],
-        div [class "backdrop-quadrant backdrop-bottom backdrop-right"] []
-      ]
-    ] ++ case state.currentDoc of
+    case state.currentDoc of
       Nothing -> []
       Just currentDoc ->
         [
