c5b9813f96eec1ea4149aaa9fec467909ef49936
Richard Feldman <richard@noredink.com>
2014-12-03 23:05:07 -0800

    Refactor RightSidebar to use LocalChannels.

--- Dreamwriter/View/RightSidebar.elm
+++ Dreamwriter/View/RightSidebar.elm
@@ -8,27 +8,42 @@ import Html.Attributes (..)
 import Html.Events (..)
 import Html.Lazy (..)
 import Maybe
-import Signal (send)
+import LocalChannel (send, LocalChannel)
 import List (..)
 
-view : Doc -> AppState -> Html
-view currentDoc state =
-  let {sidebarBody, sidebarFooter} = case state.currentNote of
+type alias Channels = {
+  newNote     : LocalChannel (),
+  searchNotes : LocalChannel (),
+  update      : LocalChannel Update
+}
+
+type alias Model = {
+  currentNote : Maybe Note,
+  notes       : List Note
+}
+
+type Update
+  = NoOp
+  | CurrentNoteChange (Maybe Note)
+
+view : Channels -> Model -> Html
+view channels model =
+  let {sidebarBody, sidebarFooter} = case model.currentNote of
     Nothing ->
-      { sidebarBody   = lazy viewNoteListings state.notes
+      { sidebarBody   = lazy2 viewNoteListings channels.update model.notes
       , sidebarFooter = span [] []
       }
     Just currentNote ->
-      { sidebarBody   = lazy viewCurrentNoteBody   currentNote
-      , sidebarFooter = lazy viewCurrentNoteFooter currentNote
+      { sidebarBody   = lazy  viewCurrentNoteBody            currentNote
+      , sidebarFooter = lazy2 viewCurrentNoteFooter channels currentNote
       }
   in
     div [id "right-sidebar-container", class "sidebar"] [
       div [id "right-sidebar-header", class "sidebar-header"] [
         input [id "notes-search-text", class "sidebar-header-control", placeholder "search notes",
-          onKeyUp (\_ -> send searchNotesChannel ())] [],
+          onKeyUp (\_ -> send channels.searchNotes ())] [],
         span [id "notes-search-button", class "sidebar-header-control flaticon-pencil90",
-          onClick <| send newNoteChannel ()] []
+          onClick <| send channels.newNote ()] []
       ],
       div [id "right-sidebar-body", class "sidebar-body"] [
         sidebarBody
@@ -36,13 +51,13 @@ view currentDoc state =
       sidebarFooter
     ]
 
-viewNoteListings notes =
-  div [id "note-listings"] <| map viewNoteListing notes
+viewNoteListings updateChannel notes =
+  div [id "note-listings"] <| map (viewNoteListing updateChannel) notes
 
-viewNoteListing : Note -> Html
-viewNoteListing note =
+viewNoteListing : LocalChannel Update -> Note -> Html
+viewNoteListing updateChannel note =
   div [key ("note-" ++ note.id), class "note-listing",
-    onClick <| send actions (SetCurrentNote (Just note))] [
+    onClick <| send updateChannel (CurrentNoteChange (Just note))] [
       div [class "flaticon-document127 note-listing-icon"] [],
       div [class "note-listing-title"] [text note.title]
     ]
@@ -56,8 +71,8 @@ viewCurrentNoteBody note =
     div [id "current-note-body"] []
   ]
 
-viewCurrentNoteFooter : Note -> Html
-viewCurrentNoteFooter note =
+viewCurrentNoteFooter : Channels -> Note -> Html
+viewCurrentNoteFooter channels note =
   div [id "current-note-controls", class "sidebar-footer"] [
     span [id "download-current-note",
       title "Download Note",
